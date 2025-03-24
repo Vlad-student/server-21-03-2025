ff998f9fd57311bd7b13062dd060dd2152569f73
Vlad-student <vlad.v.terletskiy@gmail.com>
2025-03-24 18:24:06 +0200

    request

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res)=>{
-    res.end('hello from express')
-});
+// app.get('/products/:id', (req, res) =>{
+// const requestStr = `method: ${req.method}, <br>
+// path: ${req.path}, <br>
+// query: ${JSON.stringify(req.query)}, <br>
+// url: ${req.url}, <br>
+// hostname: ${req.hostname}, <br>
+// params: ${JSON.stringify(req.params)}, <br>
+// key: ${req.id}
+// `; 
+// res.end(`<h1>${requestStr}</h1>`);
+// });
 
-app.get('/about', (req, res)=>{
-    res.end('page about')
-});
-
-app.get('/contact', (req, res)=>{
-    res.end('page contact')
-});
-
-
-
-app.use((req, res)=>{
-    res.end('error 404')
+app.get('/users/:userId', (req, res) =>{
+    res.end(`<h1> ${req.path} <br> ${req.params.userId} </h1>`)
 });
 
 module.exports = app;
